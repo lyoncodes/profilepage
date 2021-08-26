9e41624f8b4ad39512443976ec0d0bdaa7427560
Michael Lyon <lyoncodes@gmail.com>
2021-08-26 13:00:57 -0700

    scaffold

---
+++ server.js
@@ -0,0 +1,25 @@
+const fastify = require('fastify')({ logger: true })
+const path = require('path')
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+})
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    pug: require('pug')
+  }
+})
+
+fastify.register(require('./App'))
+
+const start = async () => {
+  try {
+    await fastify.listen(8080)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
